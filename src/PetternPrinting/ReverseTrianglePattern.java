01b52e6129897f8570dba8dde52c307cf4eb7c95
AvneshTech <avneshbaghel.30july@gmail.com>
2026-04-21 12:19:53 +0530

    Reverse Triangle Pattern

--- src/PetternPrinting/ReverseTrianglePattern.java
+++ src/PetternPrinting/ReverseTrianglePattern.java
@@ -1,4 +1,15 @@
 package PetternPrinting;
-
+import java.util.Scanner;
 public class ReverseTrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            for (int j = i+1; j > 0; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
