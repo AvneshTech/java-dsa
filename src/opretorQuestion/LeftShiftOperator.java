f94d593e4d0cf8190150147ad1d8ba7ec12d58c6
AvneshTech <avneshbaghel.30july@gmail.com>
2026-04-21 11:54:57 +0530

    Operator Question Added

---
+++ src/opretorQuestion/LeftShiftOperator.java
@@ -0,0 +1,5 @@
+package opretorQuestion;
+
+public class LeftShiftOperator {
+
+}
---
+++ src/opretorQuestion/XorOperator.java
@@ -0,0 +1,9 @@
+package opretorQuestion;
+
+public class XorOperator {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 8;
+        System.out.println(a ^ b);
+    }
+}
