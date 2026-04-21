e0ad50d3c92a5f48d0f77792c5e87a13707aef8f
AvneshTech <avneshbaghel.30july@gmail.com>
2026-04-21 16:35:16 +0530

    Segregate code updated

--- src/Arrays/Segregete0sand1s.java
+++ src/Arrays/Segregete0sand1s.java
@@ -3,12 +3,33 @@ package Arrays;
 public class Segregete0sand1s {
     public static void main(String[] args) {
         int[] arr = {0,1,0,1,0,1,0,1};
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == 0){
-                count++;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left < right){
+
+            while(arr[left] == 0 && left < right){
+                left++;
+            }
+
+            while(arr[right] == 1 && left < right){
+                right--;
+            }
+
+            if(left < right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
             }
         }
-        System.out.println(count);
+
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+
     }
 }
