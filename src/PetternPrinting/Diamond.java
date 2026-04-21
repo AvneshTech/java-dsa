d7ca9cbd5ddb038f5de59495344edfbcfa9ae25b
AvneshTech <avneshbaghel.30july@gmail.com>
2026-04-21 15:23:11 +0530

    Diamond Array Code Addded

---
+++ src/PetternPrinting/Diamond.java
@@ -0,0 +1,4 @@
+package PetternPrinting;
+
+public class Diamond {
+}
---
+++ src/PetternPrinting/Zeroand1SPattern.java
@@ -0,0 +1,18 @@
+package PetternPrinting;
+import java.util.Scanner;
+
+public class Zeroand1SPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((i + j) % 2 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
