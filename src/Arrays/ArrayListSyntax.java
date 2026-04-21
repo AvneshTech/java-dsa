d2737187174c52729e2998f334119886f0f13d1a
AvneshTech <avneshbaghel.30july@gmail.com>
2026-04-21 18:23:20 +0530

    ArrayListSyntax added

---
+++ src/Arrays/ArrayListSyntax.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListSyntax {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr= new ArrayList<>();
+        arr.add(32);
+        arr.add(23);
+        arr.add(45);
+        System.out.println(arr.size());
+    }
+}
