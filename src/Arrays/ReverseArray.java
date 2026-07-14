f5857c6d792656de388c3e5e85f08ad904fdd679
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-14 21:57:24 +0530

    reverse array second approch

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -4,17 +4,22 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] arr = {4,5,6,4,8,9,67,34};
         int n = arr.length;
-        int i = 0, j = n-1;
+//        int i = 0, j = n-1;
+//
+//        while(i<j){
+//            int temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+//            i++;
+//            j--;
+//        }
+//        for(int k : arr){
+//            System.out.print(k+" ");
+//        }
 
-        while(i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
-        for(int k : arr){
-            System.out.print(k+" ");
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(arr[i]+" ");
         }
     }
 }
+
