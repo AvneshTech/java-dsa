d185a4e2ab382697bbb481ef0e56f95027e23485
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-16 23:02:58 +0530

    Reverse word using function

--- src/Arrays/reverseSentence.js
+++ src/Arrays/reverseSentence.js
@@ -1,9 +1,15 @@
-// without using any functions
-let sentence = "JavaScript awesome";
-let words = sentence.split(" ");
-let result = "";
+// // without using any functions
+// let sentence = "JavaScript awesome";
+// let words = sentence.split(" ");
+// let result = "";
+//
+// for (let i = words.length - 1; i >= 0; i--){
+//     result += words[i] + " ";
+// }
+// console.log(result.trim());
 
-for (let i = words.length - 1; i >= 0; i--){
-    result += words[i] + " ";
-}
-console.log(result.trim());
+// using fucntion
+
+let sentence = "Everything is okay";
+let reversed = sentence.split(" ").reverse().join(" ");
+console.log(reversed);
