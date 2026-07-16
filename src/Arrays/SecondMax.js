f9ccca9b0480bbfcd42cfec8fb5d908ccda15335
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-16 23:15:42 +0530

    Second Max in js

---
+++ src/Arrays/SecondMax.js
@@ -0,0 +1,15 @@
+let Arr= [3,6,7,8,6,4,0,23];
+
+let max = Integer.MIN_VALUE;
+let secondMax = Integer.MIN_VALUE;
+
+for(let i = 0; i < Arr.length; i ++){
+    if (Arr[i] > max){
+        secondMax = max ;
+        max = Arr[i];
+    }
+     else if (Arr[i] > secondMax && Arr[i] != max){
+         secondMax = Arr[i];
+    }
+}
+console.log(secondMax);
