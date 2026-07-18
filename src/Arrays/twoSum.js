2528aa7fab406d4489f7b8f58ff6b17ce29b7388
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-18 23:17:42 +0530

    Fix

--- src/Arrays/twoSum.js
+++ src/Arrays/twoSum.js
@@ -5,11 +5,9 @@
     let target = 10;
 
     for(let i = 0; i<arr.length; i++){
-
-
-    for(let j = i+1; j<arr.length; j++) {
+       for(let j = i+1; j<arr.length; j++) {
         if (arr[i] + arr[j] == target) {
             console.log (arr[i]+" "+arr[j]);
         }
-    }
+      }
     }
