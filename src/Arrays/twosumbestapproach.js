5b3ad08d88fab3aa4be9e32a4512ab6c00514c49
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-18 23:22:22 +0530

    Best Approach

---
+++ src/Arrays/twosumbestapproach.js
@@ -0,0 +1,1 @@
+let arr = [1,2,3,4,5,6,7,8,9,10]; let target = 10; let seen = new Set(); for (let num of arr) { let complement = target - num; if (seen.has(complement)) { console.log(complement, num); } seen.add(num); }
