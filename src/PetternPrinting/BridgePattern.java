aad3d8329d699bf2246716ca614bd0d5b7ec0e04
AvneshTech <avneshbaghel.30july@gmail.com>
2026-09-25 18:10:27 +0530

    Bridge Pattern

---
+++ src/PetternPrinting/BridgePattern.java
@@ -0,0 +1,21 @@
+package PetternPrinting;
+import java.util.Scanner;
+
+public class BridgePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the Nymber\n");
+        n = sc.nextInt();
+
+        for (int i = 1; i <=2*n-1; i++){
+            System.out.println("* ");
+        }
+        System.out.println( );
+        for (int i = 1; i < n; i++){
+            for (int j = 1)
+        }
+    }
+
+
+}
